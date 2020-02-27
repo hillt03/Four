99da2921e7b864e1f2ac5543d58027414feef499
hillt03 <hillt03@yahoo.com>
2020-02-27 07:26:44 -0500

    working bot with translator

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -1,13 +1,11 @@
 import unittest
 import repackage
 repackage.up()
+import four.timlatin.timlatin
 
 
 class TestModules(unittest.TestCase):
-    pass
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+    def test_timlatin_translation(self):
+        translator = four.timlatin.timlatin.TimlatinTranslator("test_timlatin.json")
+        self.assertEqual(translator.translate_text("hello how are you"), "henlo how are you")
+        self.assertEqual(translator.translate_text("this is a test"), "thins ins a abc123")
