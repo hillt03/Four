df0e013847d8079b41caa77d8ae22237318b8c81
hillt03 <hillt03@yahoo.com>
2020-03-01 23:25:46 -0500

    updated cosmos

--- four/bot/cogs/misc.py
+++ four/bot/cogs/misc.py
@@ -17,14 +17,14 @@ class Misc(commands.Cog):
             await ctx.send("Data unavailable.")
         astros = astros.json()
 
-        description = "There are currently" + str(astros["number"]) + " astronauts in space."
+        description = "There are currently " + str(astros["number"]) + " astronauts in space."
         embed = discord.Embed(title="Astronauts In Space", description=description, color=0xfff78c)
         embed.set_image(url="https://i.imgur.com/zReLsRn.png")
         embed.set_author(name="Four", icon_url="https://i.imgur.com/6Cqkof8.png")
         embed.set_thumbnail(url="https://i.imgur.com/6Cqkof8.png")
         counter = 1
         for astro in astros["people"]:
-            value = f"Craft: {astro['craft']}\nName: {astro['name']}"
+            value = f"{astro['name']}\nCraft: {astro['craft']}"
             embed.add_field(name="Astronaut " + str(counter), value=value, inline=False)
             counter += 1
         await ctx.send(embed=embed)
